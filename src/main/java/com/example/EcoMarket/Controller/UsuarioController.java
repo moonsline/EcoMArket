3fac5bb6d68d45d595aa3f7931ce1db377d605d7
Ariel Silva Ortega <ar.silvao@duocuc.cl>
2025-05-01 15:47:49 -0400

    Adición de UsuarioController pero con error al conectar :(

---
+++ src/main/java/com/example/EcoMarket/Controller/UsuarioController.java
@@ -0,0 +1,45 @@
+package com.example.EcoMarket.Controller;
+
+
+import com.example.EcoMarket.Model.Model_Usuario;
+import com.example.EcoMarket.Service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public List<Model_Usuario> obtenerUsuarios(){
+        return usuarioService.getUsuarios();
+    }
+
+    @PostMapping
+    public Model_Usuario guardarUsuario(@RequestBody Model_Usuario usuario){
+        return usuarioService.saveUsuario(usuario);
+    }
+
+    @GetMapping("{id}")
+    public Model_Usuario buscarPorId(@PathVariable int id){
+        return usuarioService.getUsuarioId(id);
+    }
+
+    @PutMapping("{id}")
+    public Model_Usuario modificarUsuario(@PathVariable int id, @RequestBody Model_Usuario usuario){
+        return usuarioService.updateUsuario(usuario);
+    }
+
+    @DeleteMapping("{id}")
+    public String eliminarUsuario(@PathVariable int id){
+        return usuarioService.deleteUsuario(id);
+    }
+
+
+
+}
