a1a0e6cc2fa8c949cb9bb0c69cd9bea510b94f74
Ariel Silva Ortega <ar.silvao@duocuc.cl>
2025-05-17 15:12:41 -0400

    Agregué controlador de proveedor
    lo había olvidado

---
+++ src/main/java/com/example/EcoMarket/Controller/ProveedorController.java
@@ -0,0 +1,40 @@
+package com.example.EcoMarket.Controller;
+
+
+import com.example.EcoMarket.Model.Model_Proveedor;
+import com.example.EcoMarket.Service.ProveedorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/proveedores")
+public class ProveedorController {
+
+    @Autowired
+    private ProveedorService proveedorService;
+
+    @GetMapping
+    public String getProveedores() {
+        return proveedorService.listarProveedor();
+    }
+
+    @PostMapping
+    public String postProveedores(@RequestBody Model_Proveedor proveedor) {
+        return proveedorService.agregarProveedor(proveedor);
+    }
+
+    @GetMapping("/{id}")
+    public String getProveedoreById(@PathVariable int id) {
+        return proveedorService.obtenerProveedor(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteProveedoreById(@PathVariable int id) {
+        return proveedorService.eliminarProveedor(id);
+    }
+
+    @PutMapping("/{id}")
+    public String upddateProveedoreById(@PathVariable int id, @RequestBody Model_Proveedor proveedor) {
+        return proveedorService.actualizarProveedor(id, proveedor);
+    }
+}
