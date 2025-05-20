a1e896a1710dcacda1c164a40bb65a7975a7ec3b
moonsline <ca.garridot@duocuc.cl>
2025-05-20 10:05:38 -0400

    Clase cliente lista para interactuar con Postman.

--- src/main/java/com/example/EcoMarket/Controller/ClienteController.java
+++ src/main/java/com/example/EcoMarket/Controller/ClienteController.java
@@ -2,11 +2,37 @@ package com.example.EcoMarket.Controller;
 
 
 import com.example.EcoMarket.Model.Model_Cliente;
+import com.example.EcoMarket.Service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cliente")
 public class ClienteController {
+    @Autowired
+    private ClienteService clienteService;
+
+     @GetMapping
+    public String getCliente() { return clienteService.listarCliente();}
+
+     @PostMapping
+    public String postCliente(@RequestBody Model_Cliente cliente) {return clienteService.agregarCliente(cliente);}
+
+    @GetMapping("/{idCliente}")
+    public String getClienteById(@PathVariable int idCliente) {
+        return clienteService.obtenerCliente(idCliente);
+    }
+
+    @DeleteMapping("/{idCliente}")
+    public String deleteClienteById(@PathVariable int idCliente) {
+        return clienteService.eliminarCliente(idCliente);
+    }
+
+    @PutMapping("/{idCliente}")
+    public String upddateClienteById(@PathVariable int idCliente, @RequestBody Model_Cliente cliente) {
+        return clienteService.actualizarCliente(idCliente, cliente);
+    }
+
+
 
 }
--- src/main/java/com/example/EcoMarket/Service/ClienteService.java
+++ src/main/java/com/example/EcoMarket/Service/ClienteService.java
@@ -1,4 +1,78 @@
 package com.example.EcoMarket.Service;
 
+
+import com.example.EcoMarket.Model.Model_Cliente;
+import com.example.EcoMarket.Repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ClienteService {
+
+    @Autowired
+    ClienteRepository clienteRepository;
+
+    public String agregarCliente(Model_Cliente cliente) {
+        clienteRepository.save(cliente);
+        return "Cliente agregado";
+    }
+    public String listarCliente() {
+        String output ="";
+        for (Model_Cliente cliente : clienteRepository.findAll()){
+            output += "ID Cliente:" +cliente.getIdCliente()+ "\n";
+            output += "Nombre:" +cliente.getNombre()+ "\n";
+            output += "Email:" +cliente.getEmail()+ "\n";
+            output += "Rol:" +cliente.getRol()+ "\n";
+            output += "Direccion:" +cliente.getDireccion()+ "\n";
+        }
+        if (output.isEmpty()){
+            return "No se encontro el cliente";
+         }else {
+            return output;
+        }
+    }
+
+    public String obtenerCliente(int idCliente) {
+        String output ="";
+        if (clienteRepository.existsById(idCliente)){
+            Model_Cliente cliente = clienteRepository.findById(idCliente).get();
+            output = "ID Cliente:" +cliente.getIdCliente()+ "\n";
+            output += "Nombre:" +cliente.getNombre()+ "\n";
+            output += "Email:" +cliente.getEmail()+ "\n";
+            output += "Rol:" +cliente.getRol()+ "\n";
+            output += "Direccion:" +cliente.getDireccion()+ "\n";
+            return output;
+        }else{
+            return "No se encontro el cliente";
+        }
+    }
+
+    public String eliminarCliente(int idCliente) {
+        if (clienteRepository.existsById(idCliente)){
+            clienteRepository.deleteById(idCliente);
+            return "Cliente eliminado";
+        }else{
+            return "No se encontro el cliente";
+        }
+    }
+
+    public String actualizarCliente(int idClinete, Model_Cliente cliente) {
+        if (clienteRepository.existsById(idClinete)){
+            Model_Cliente buscado = clienteRepository.findById(idClinete).get();
+            buscado.setNombre(cliente.getNombre());
+            buscado.setEmail(cliente.getEmail());
+            buscado.setRol(cliente.getRol());
+            buscado.setDireccion(cliente.getDireccion());
+            clienteRepository.save(buscado);
+            return "Cliente actualizado";
+        }else{
+            return "No se encontro el cliente";
+        }
+    }
+
+
+
+
+
+
 }
