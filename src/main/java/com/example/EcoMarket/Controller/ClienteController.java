1e9f995b39d3a6ebf800223eaf8665b223312654
moonsline <ca.garridot@duocuc.cl>
2025-05-20 09:28:49 -0400

    Clases service creadas

--- src/main/java/com/example/EcoMarket/Controller/ClienteController.java
+++ src/main/java/com/example/EcoMarket/Controller/ClienteController.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/Cliente")
+@RequestMapping("/cliente")
 public class ClienteController {
-
-
 
 }
