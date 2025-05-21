25b0e069341b56b92475b476706e908a9c657fb8
Ariel Silva Ortega <ar.silvao@duocuc.cl>
2025-05-21 11:18:40 -0400

    corrección a /direccion
    normalicé los nombres de las direcciones para facilitar pruebas y corregí errores de tipeo en estas

--- src/main/java/com/example/EcoMarket/Controller/ClienteController.java
+++ src/main/java/com/example/EcoMarket/Controller/ClienteController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/cliente")
+@RequestMapping("/clientes")
 public class ClienteController {
     @Autowired
     private ClienteService clienteService;
--- src/main/java/com/example/EcoMarket/Controller/GerenteTiendaController.java
+++ src/main/java/com/example/EcoMarket/Controller/GerenteTiendaController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/gerente")
+@RequestMapping("/gerentes")
 public class GerenteTiendaController {
 
     @Autowired
