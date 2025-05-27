e5b5a7a7b18816e8835146cb87c8ab883d6dd9e6
Carolina Garrido T. <ca.garridot@duocuc.cl>
2025-05-27 10:09:29 -0400

    Creacion de Branch Test, Application proterties dev y test, y clase ExampleProductoTest. Clase ExampleProductoTest comentada.

---
+++ src/test/java/com/example/EcoMarket/ExampleProductoTest.java
@@ -0,0 +1,36 @@
+package com.example.EcoMarket;
+
+import com.example.EcoMarket.Model.Model_Producto;
+import com.example.EcoMarket.Repository.ProductoRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/*
+
+public class ExampleProductoTest {
+    @DisplayName("FindAll Test")
+        void testProductServiceMock(){
+            List<Model_Producto> productos = ProductoRepository.findAll();
+            assertNotNull(productos);
+            assertEquals(expected:1, productos.size());
+
+    }
+
+    @Test
+    @DisplayName("Test")
+    void testFindProduct(){
+        Model_Producto prueba = ProductoRepository.findById(1).get();
+        assertNotNull(prueba);
+        assertEquals(prueba.getNombre(), prueba.getNombre());
+
+    }
+}
+
+*/
+
+
