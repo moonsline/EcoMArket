55837d564b3a202c64eb0927e9054a6fd596cacf
Ariel Silva Ortega <ar.silvao@duocuc.cl>
2025-06-02 18:38:23 -0400

    Actualización Test
    completé lo que está en ProductoTest

--- src/test/java/com/example/EcoMarket/ExampleProductoTest.java
+++ src/test/java/com/example/EcoMarket/ExampleProductoTest.java
@@ -3,17 +3,22 @@ package com.example.EcoMarket;
 import com.example.EcoMarket.Model.Model_Producto;
 import com.example.EcoMarket.Repository.ProductoRepository;
 import com.example.EcoMarket.Service.ProductoService;
+import com.example.EcoMarket.Controller.ProductoController;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -25,28 +30,54 @@ public class ExampleProductoTest {
     @Autowired
     ProductoRepository productoRepository;
 
+    @Autowired
+    MockMvc mockMvc;
+
     @MockitoBean
-    ProductoService  productServiceMock;
+    ProductoService productServiceMock;
 
-
+    @Test
     @DisplayName("FindAll Test")
-        void testProductServiceMock(){
-            List<Model_Producto> productos = productoRepository.findAll();
-            assertNotNull(productos);
-            assertEquals(1,productos.size());
+    void testProductServiceMock() {
+        List<Model_Producto> productos = productoRepository.findAll();
+        assertNotNull(productos);
+        assertEquals(1, productos.size());
 
     }
 
     @Test
-    @DisplayName("Test findProduct")
-    void testFindProduct(){
+    @DisplayName("Rectificar precio producto")
+    void testFindProduct() {
         Model_Producto prueba = productoRepository.findById(1).get();
         assertNotNull(prueba);
         assertEquals(prueba.getNombre(), prueba.getNombre());
 
     }
+
+    @Test
+    @DisplayName("Test controller")
+    void testController() {
+        //Indicamos que el retorno de listarProductos se identificara con el valor ingresado en thenReturn
+        when(productServiceMock.listaProducto()).thenReturn("Lista completa");
+
+        //Bloque Try Except/Catch
+        //Nos permite probar una funcionalidad de codigo o un segmento de codigo y si este falla
+        //Se captura por medio de Catch(Exception var) y ejecuta un control de error
+        try {
+            //MockMvc Nos permite realizar consultas HTTPMethod
+            //perform nos permite ejecutar dichas llamadas y luego ingresamos el metodo HTTP correspondiente
+            //adicionalmente podemos agregar parametros u variables adicionales de ser requerido
+            //Finalmente andExcept nos permite indicar que esperamos de dicha respuesta HTTP
+            //Tanto codigo como contenido
+            mockMvc.perform(get("/productos"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().string("Lista completa"));
+
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            fail();
+        }
+    }
 }
-
-
 
 
