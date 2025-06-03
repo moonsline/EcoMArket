12307f24d36da9bf0db3d27babe4ecda24328f9d
albaescobar <al.escobarf@duocuc.cl>
2025-06-03 09:53:49 -0400

    cliente-administrador

--- src/main/java/com/example/EcoMarket/Controller/AdministradorController.java
+++ src/main/java/com/example/EcoMarket/Controller/AdministradorController.java
@@ -3,16 +3,22 @@ package com.example.EcoMarket.Controller;
 
 import com.example.EcoMarket.Model.Model_Administrador;
 import com.example.EcoMarket.Service.AdministradorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/administradores")
+@Tag(name="Administrador Controller", description = "Servicio de Gestión de cliente")
 public class AdministradorController {
     @Autowired
     private AdministradorService administradorService;
 
     @GetMapping
+    @Operation(summary = "Administrador", description = "Obtiene el administrador y su información")
+    @ApiResponse(responseCode = "200", description = "¡Consulta Exitosa!")
     public String getAdministrador() { return administradorService.listaAdminstrador();}
 
     @PostMapping
