b5000a1e935191bf5c09345441f2ac64959c4323
Ariel Silva Ortega <ar.silvao@duocuc.cl>
2025-06-11 20:12:33 -0400

    avance pruebas unitarias
    cada vez quedan menos :'(

--- src/test/java/com/example/EcoMarket/ExampleGerenteTiendaTest.java
+++ src/test/java/com/example/EcoMarket/ExampleGerenteTiendaTest.java
@@ -1,4 +1,75 @@
 package com.example.EcoMarket;
 
+
+import com.example.EcoMarket.Model.Model_EmpleadoVentas;
+import com.example.EcoMarket.Model.Model_GerenteTienda;
+import com.example.EcoMarket.Repository.GerenteTiendaRepository;
+import com.example.EcoMarket.Service.GerenteTiendaService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ExampleGerenteTiendaTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private GerenteTiendaRepository gerenteTiendaRepository;
+    @Autowired
+    private GerenteTiendaService gerenteTiendaServiceMock;
+
+    @Test
+    @DisplayName("FindAll test")
+    void testGerenteTiendaServiceMock(){
+        List<Model_GerenteTienda> gerenteTiendas = gerenteTiendaRepository.findAll();
+        assertNotNull(gerenteTiendas);
+        assertEquals(1, gerenteTiendas.size());
+    }
+
+    @Test
+    @DisplayName("Rectificar nombre empleadoVentas")
+    void testFindGerenteTienda(){
+        Model_GerenteTienda prueba = gerenteTiendaRepository.findById(1).get();
+        assertNotNull(prueba);
+        assertEquals("Orlando Sepulveda", prueba.getNombre());
+    }
+
+    @Test
+    @DisplayName("Test controller")
+    void testController(){
+        when(gerenteTiendaServiceMock.listarGerenteTienda()).thenReturn("Lista completa");
+
+        try{
+            mockMvc.perform(get("/gerentes"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().string("Lista completa"));
+        } catch (Exception ex){
+            System.out.println(ex.getMessage());
+            fail();
+        }
+
+    }
+
+    @Test
+    @DisplayName("Actualizar nombre gerenteTienda")
+    void testUpdateGerenteTienda(){
+        Model_GerenteTienda prueba = gerenteTiendaRepository.findById(1).get();
+        assertNotNull(prueba);
+        prueba.setNombre("Orlandios Sepulveda");
+        gerenteTiendaRepository.save(prueba);
+        Model_GerenteTienda gerenteTiendaActualizado = gerenteTiendaRepository.findById(1).get();
+        assertEquals("Orlandios Sepulveda", gerenteTiendaActualizado.getNombre());
+    }
 }
--- src/test/java/com/example/EcoMarket/ExampleLogisticaTest.java
+++ src/test/java/com/example/EcoMarket/ExampleLogisticaTest.java
@@ -1,4 +1,78 @@
 package com.example.EcoMarket;
 
+
+import com.example.EcoMarket.Model.Model_GerenteTienda;
+import com.example.EcoMarket.Model.Model_Logistica;
+import com.example.EcoMarket.Repository.LogisticaRepository;
+import com.example.EcoMarket.Service.LogisticaService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ExampleLogisticaTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private LogisticaRepository logisticaRepository;
+    @Autowired
+    private LogisticaService logisticaServiceMock;
+
+    @Test
+    @DisplayName("FindAll test")
+    void testLogisticaServiceMock(){
+        List<Model_Logistica> logistica = logisticaRepository.findAll();
+        assertNotNull(logistica);
+        assertEquals(1, logistica.size());
+    }
+
+    @Test
+    @DisplayName("Rectificar nombre logistica")
+    void testFindLogistica(){
+        Model_Logistica prueba = logisticaRepository.findById(1).get();
+        assertNotNull(prueba);
+        assertEquals("Pedro Pablo Perez", prueba.getNombre());
+    }
+
+    @Test
+    @DisplayName("Test controller")
+    void testController(){
+        when(logisticaServiceMock.listarLogistica()).thenReturn("Lista completa");
+
+        try{
+            mockMvc.perform(get("/logistica"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().string("Lista completa"));
+        } catch (Exception ex){
+            System.out.println(ex.getMessage());
+            fail();
+        }
+
+    }
+
+    @Test
+    @DisplayName("Actualizar nombre logistica")
+    void testUpdateLogistica(){
+        Model_Logistica prueba = logisticaRepository.findById(1).get();
+        assertNotNull(prueba);
+        prueba.setNombre("Gonzalo Gonzales Ganzo");
+        logisticaRepository.save(prueba);
+        Model_Logistica logisticaActualizado = logisticaRepository.findById(1).get();
+        assertEquals("Gonzalo Gonzales Ganzo", logisticaActualizado.getNombre());
+    }
 }
