d041aa22de2c8c7d5c2ccf32af2e61efbfdb492a
Ariel Silva Ortega <ar.silvao@duocuc.cl>
2025-06-13 14:27:06 -0400

    Update ExamplePedidoTest.java

--- src/test/java/com/example/EcoMarket/ExamplePedidoTest.java
+++ src/test/java/com/example/EcoMarket/ExamplePedidoTest.java
@@ -1,4 +1,49 @@
 package com.example.EcoMarket;
 
+import com.example.EcoMarket.Model.Model_Pedido;
+import com.example.EcoMarket.Repository.PedidoRepository;
+import com.example.EcoMarket.Service.PedidoService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ExamplePedidoTest {
+
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    private PedidoService pedidoServiceMock;
+
+    @Test
+    @DisplayName("FindAll Test")
+    void testPedidoServiceMock(){
+        List<Model_Pedido> pedidos = pedidoRepository.findAll();
+        assertNotNull(pedidos);
+        assertEquals(1, pedidos.size());
+    }
+
+    @Test
+    @DisplayName("Rectificar estado del pedido")
+    void testFindPedido(){
+        Model_Pedido prueba = pedidoRepository.findAllById(1).get();
+        assertNotNull(prueba);
+        assertEquals("activo", prueba.getEstado());
+    }
+
+    
 }
