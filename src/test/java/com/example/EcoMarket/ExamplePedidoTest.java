b3fb6de90e6abc7dc677dc70170e1642d88faa78
Ariel Silva Ortega <ar.silvao@duocuc.cl>
2025-06-13 16:09:44 -0400

    pedidotest listo
    actualicé métodos para pruebas unitarias, ahora se puede cambiar el estado del pedido de activo a terminado por medio de la prueba.

--- src/test/java/com/example/EcoMarket/ExamplePedidoTest.java
+++ src/test/java/com/example/EcoMarket/ExamplePedidoTest.java
@@ -1,5 +1,6 @@
 package com.example.EcoMarket;
 
+import com.example.EcoMarket.Model.Model_Administrador;
 import com.example.EcoMarket.Model.Model_Pedido;
 import com.example.EcoMarket.Repository.PedidoRepository;
 import com.example.EcoMarket.Service.PedidoService;
@@ -40,10 +41,52 @@ public class ExamplePedidoTest {
     @Test
     @DisplayName("Rectificar estado del pedido")
     void testFindPedido(){
-        Model_Pedido prueba = pedidoRepository.findAllById(1).get();
+        Model_Pedido prueba = pedidoRepository.findById(1).get();
         assertNotNull(prueba);
         assertEquals("activo", prueba.getEstado());
     }
+    @Test
+    @DisplayName("Test controller")
+    void testController() {
+        //Indicamos que el retorno de listarAdministradores se identificara con el valor ingresado en thenReturn
+        when(pedidoServiceMock.listarPedidos()).thenReturn("Lista completa");
 
-    
+        //Bloque Try Except/Catch
+        //Nos permite probar una funcionalidad de codigo o un segmento de codigo y si este falla
+        //Se captura por medio de Catch(Exception var) y ejecuta un control de error
+        try {
+            //MockMvc Nos permite realizar consultas HTTPMethod
+            //perform nos permite ejecutar dichas llamadas y luego ingresamos el metodo HTTP correspondiente
+            //adicionalmente podemos agregar parametros u variables adicionales de ser requerido
+            //Finalmente andExcept nos permite indicar que esperamos de dicha respuesta HTTP
+            //Tanto codigo como contenido
+            mockMvc.perform(get("/pedidos"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().string("Lista completa"));
+
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            fail();
+        }
+    }
+
+    @Test
+    @DisplayName("Actualizar nombre admin")
+    void testUpdatePedido() {
+        // Buscar el pedido por su ID
+        Model_Pedido pedido = pedidoRepository.findById(1).get();
+
+        // Verificar que el pedido existe
+        assertNotNull(pedido);
+
+        // Actualizar el estado del pedido
+        pedido.setEstado("terminado");
+        pedidoRepository.save(pedido);
+
+        // Recuperar el producto actualizado
+        Model_Pedido pedidoActualizado = pedidoRepository.findById(1).get();
+
+        // Verificar que el nombre se haya actualizado correctamente
+        assertEquals("terminado", pedidoActualizado.getEstado());
+    }
 }
